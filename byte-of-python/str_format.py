296b9ec1788740e73c25e74387ca3538fb3d327b
pam-phy <pam.phys.th@gamil.com>
2017-08-27 16:50:31 +0800

    study

---
+++ byte-of-python/str_format.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with python?'.format(name))
