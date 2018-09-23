3567efe3e2b7d8c915b4f5c466452bf66508a1b3
pam-phy <pam.phys.th@gmail.com>
2018-09-23 16:47:03 +0800

    更新

---
+++ tutorial/func_ask_ok.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+def ask_ok(prompt, retries=4, remainder='Please try again!'):
+	while True:
+		ok = input(prompt)
+		if ok in ('y', 'ye', 'yes'):
+			return True
+		if ok in ('n', 'no', 'nop', 'nope'):
+			return False
+		retries = retries - 1
+		if retries < 0:
+			raise ValueError('Invalid user response')
+		print(remainder)
